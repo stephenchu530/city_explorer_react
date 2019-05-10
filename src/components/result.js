868e36eba8fb2d016c82c85d61a2629d58281ea4
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-10 15:52:17 -0700

    refactor: made the results code more dry

--- src/components/result.js
+++ src/components/result.js
@@ -12,6 +12,7 @@ class Result extends React.Component {
       data: null
     };
   }
+
   componentDidMount(){
     let URL = `${this.props.backend}/${this.props.resource}`;
     superagent.get(URL)
@@ -21,27 +22,14 @@ class Result extends React.Component {
         this.setState({ data });
       });
   }
+
   render() {
     return (
       <section>
         <h3>Results from the { this.props.title } API</h3>
         <ul>
         <If condition={ this.state.data !== null }>
-          <If condition={ this.props.resource === 'weather' }>
-            <Weather data={ this.state.data } />
-          </If>
-          <If condition={ this.props.resource === 'events' }>
-            <Events data={ this.state.data } />
-          </If>
-          <If condition={ this.props.resource === 'yelp' }>
-            <Yelp data={ this.state.data } />
-          </If>
-          <If condition={ this.props.resource === 'movies' }>
-            <Movies data={ this.state.data } />
-          </If>
-          <If condition={ this.props.resource === 'trails' }>
-            <Trails data={ this.state.data } />
-          </If>
+          <ListItem resource={ this.props.resource } data={ this.state.data } />
         </If>
         </ul>
       </section>
@@ -49,56 +37,20 @@ class Result extends React.Component {
   }
 }
 
-class Weather extends React.Component {
-  render() {
-    let renderItems = this.props.data.map((e, i) => (<li key={ i }>The forecast for { e.time } is: { e.forecast  }</li>));
-    return (
-      <>
-        { renderItems }
-      </>
-    )
+class ListItem extends React.Component {
+  template = {
+    'weather': (e) => this.props.data.map((e, i) => (<li key={ i }>The forecast for { e.time } is: { e.forecast  }</li>)),
+    'events': (e) => this.props.data.map((e, i) => (<li key={ i }><a href={ e.link }>{ e.name }</a><p>Event Date: { e.event_date }</p><p>{ e.summary }</p></li>)),
+    'yelp': (e) => this.props.data.map((e, i) => (<li key={ i }><a href={ e.url }>{ e.name }</a><p>The average rating is { e.rating } out of 5 and the average cost is { e.price } out of 4</p><img src={ e.image_url } /></li>)),
+    'movies': (e) => this.props.data.map((e, i) => (<li key={ i }><p><span>{ e.title }</span> was relased on { e.released_on }. Out of { e.total_votes } total votes, { e.title } has an average vote of { e.average_votes } and a popularity score of { e.popularity }.</p><img src={ e.image_url } /><p>{ e.overview }</p></li>)),
+    'trails': this.props.data.map((e, i) => (<li key={ i }><p>Hike Name: <a href={ e.trail_url }>{ e.name }</a>, Location: { e.location }, Distance: { e.length } miles</p><p>On { e.condition_date } at { e.condition_time }, trail conditions were reported as: { e.conditions }</p><p>This trail has a rating of { e.stars } stars (out of { e.star_votes } votes)</p><p>{ e.summary }</p></li>))
   }
-}
 
-class Events extends React.Component {
   render() {
-    let renderItems = this.props.data.map((e, i) => (<li key={ i }><a href={ e.link }>{ e.name }</a><p>Event Date: { e.event_date }</p><p>{ e.summary }</p></li>));
+    let renderListItems = this.template[this.props.resource](this.props.data);
     return (
       <>
-        { renderItems }
-      </>
-    )
-  }
-}
-
-class Yelp extends React.Component {
-  render() {
-    let renderItems = this.props.data.map((e, i) => (<li key={ i }><a href={ e.url }>{ e.name }</a><p>The average rating is { e.rating } out of 5 and the average cost is { e.price } out of 4</p><img src={ e.image_url } /></li>));
-    return (
-      <>
-        { renderItems }
-      </>
-    )
-  }
-}
-
-class Movies extends React.Component {
-  render() {
-    let renderItems = this.props.data.map((e, i) => (<li key={ i }><p><span>{ e.title }</span> was relased on { e.released_on }. Out of { e.total_votes } total votes, { e.title } has an average vote of { e.average_votes } and a popularity score of { e.popularity }.</p><img src={ e.image_url } /><p>{ e.overview }</p></li>));
-    return (
-      <>
-        { renderItems }
-      </>
-    )
-  }
-}
-
-class Trails extends React.Component {
-  render() {
-    let renderItems = this.props.data.map((e, i) => (<li key={ i }><p>Hike Name: <a href={ e.trail_url }>{ e.name }</a>, Location: { e.location }, Distance: { e.length } miles</p><p>On { e.condition_date } at { e.condition_time }, trail conditions were reported as: { e.conditions }</p><p>This trail has a rating of { e.stars } stars (out of { e.star_votes } votes)</p><p>{ e.summary }</p></li>));
-    return (
-      <>
-        { renderItems }
+        { renderListItems }
       </>
     )
   }
