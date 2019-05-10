6d08ad2a5e46794b94a3a76f65af0b6884342dc3
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-10 15:10:22 -0700

    fix: added missing flavor text; minor css

--- src/components/map.js
+++ src/components/map.js
@@ -4,7 +4,10 @@ class Map extends React.Component {
   render() {
     let src = `https://maps.googleapis.com/maps/api/staticmap?center=${this.props.location.latitude}%2c%20${this.props.location.longitude}&zoom=13&size=600x300&maptype=roadmap%20%20&key=${localStorage.geocode}`
     return (
+      <>
         <img src={src} alt="Map of search query" />
+        <h2>Here are the results for: { this.props.location.formatted_query }</h2>
+      </>
     )
   }
 }
